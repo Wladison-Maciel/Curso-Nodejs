5adaff880795b1434686c8018c21cb8e7bb8ac4b
Wladison-Maciel <franciscowladison97@gmail.com>
2025-02-06 17:31:53 -0300

    Revisando conceito de rotas

--- dominando-nodejs/index.js
+++ dominando-nodejs/index.js
@@ -1,8 +1,31 @@
 const express = require("express");
 const server = express();
 
+// http://localhost:3000/hello?nome=felipe&idade=17
+// QUery params = ?nome=felipe&idade=17 
+
 server.get("/hello", (req, res) => {
-    return res.json({title: "Hello World"});
+
+    const { nome, idade } = req.query; // Fazendo a desconstrução dos parâmetros que eu quero (Parametros Opcionais)
+
+    return res.json({
+        title: `Olá ${nome} tudo bem?`,
+        idade: `Sua idade é: ${idade}`
+    });
+});
+
+
+// http://localhost:3000/hello/:nome
+// http://localhost:3000/hello/felipe
+
+server.get("/hello/:nome", (req, res) => {
+
+    const nome = req.params.nome;
+
+
+    return res.json({
+        title: `Olá ${nome} tudo bem?`
+    });
 });
 
 server.listen(3000);
