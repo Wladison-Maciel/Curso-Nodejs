305dfce0461e2c88003fe9e1a0a85342fc40082f
Wladison-Maciel <franciscowladison97@gmail.com>
2025-02-10 09:13:06 -0300

    Adicionando um Controller e criando pasta Controller

---
+++ dominando-nodejs/src/app/controllers/CustomersController.js
@@ -0,0 +1,24 @@
+class CustomersController {
+    // Listagem dos Customers
+    index(req, res) {
+
+    }
+    // Listagem de um Customer
+    show(req, res){
+
+    }
+    // Cria um novo Customer
+    create(req, res){
+        
+    }
+    // Atualização de um Customer
+    update(req, res){
+        
+    }
+    // Deleta um Customer
+    destroy(req, res){
+        
+    }
+}
+
+module.exports = new CustomersController();
