e90d8775c7ae4c39dcdd8c8a66351cae93fca7de
Wladison-Maciel <franciscowladison97@gmail.com>
2025-02-10 09:41:34 -0300

    Adicionando Rotas ao arquivo Routes.js

--- dominando-nodejs/src/routes.js
+++ dominando-nodejs/src/routes.js
@@ -1,10 +1,13 @@
 const { Router } = require("express"); // Importa apenas a funcionalidade "Router" do Express
 const routes = new Router(); // Cria uma instância do Router para definir rotas da aplicação
+const customers = require("./app/controllers/CustomersController")
 
-// Define uma rota GET para o caminho "/hello"
-routes.get("/hello" , (req, res) => {
-    return res.json({ message: "Hello "});
-});
+
+routes.get("/customers" , customers.index );
+routes.get("/customers/:id" , customers.show );
+routes.post("/customers" , customers.create );
+routes.put("/customers/:id" , customers.update );
+routes.delete("/customers/:id" , customers.destroy );
 
 // Exporta as rotas para serem usadas em outro arquivo
 module.exports = routes;
