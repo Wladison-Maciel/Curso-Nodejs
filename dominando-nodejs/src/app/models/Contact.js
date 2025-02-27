a60a0af47e25118b6d6b6df178154a3b56e6f2f0
Wladison-Maciel <franciscowladison97@gmail.com>
2025-02-27 19:35:54 -0300

    Modificando Contacts para Receber a coluna STATUS

--- dominando-nodejs/src/app/models/Contact.js
+++ dominando-nodejs/src/app/models/Contact.js
@@ -5,6 +5,7 @@ class Contact extends Model {
         super.init({
             name: Sequelize.STRING,
             email: Sequelize.STRING,
+            status: Sequelize.ENUM("ACTIVE", "ARCHIVED"),
         },
         {
             sequelize,
